5a11da5fc3da50b005d3649adede7c38cf590266
KunalBhoyar <kunal@Kunals-MacBook-Air.local>
2024-08-25 18:19:24 -0400

    Dockerized the App and created a docker compose for it

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,23 @@
 # Save this as Dockerfile
-FROM python:3.8-slim
+FROM python:3.9-slim
 
 #Current working directory
 WORKDIR /app
 
-#Copy the current dir code to the conatiner WORK dir
+
+# Copy the requirements file to the container
+COPY requirements.txt .
+
+
+# Install any dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code to the container
 COPY . .
 
-#Install Request
-RUN pip install requests
+# Expose the port on which the app will run
+EXPOSE 5000
 
 #Run the application
-CMD ["python", "weather_check.py"]
+CMD ["python", "app.py"]
 
--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, request
 import requests
-import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -8,21 +12,22 @@ app = Flask(__name__)
 def index():
     weather = None
     error = None
+    api_key = os.getenv('WEATHER_API_KEY')  # Load the API key from environment variables
+
     if request.method == 'POST':
         city = request.form.get('city')
+        
         if not city:
             error = "City name cannot be empty. Please enter a valid city name."
+        elif not api_key:
+            error = "No API key found! Please set the WEATHER_API_KEY environment variable."
         else:
-            api_key = os.getenv('WEATHER_API_KEY') # Replace with your WeatherAPI key
-            
             url = f'http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no'
-
             try:
                 response = requests.get(url)
-                response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
+                response.raise_for_status()
                 data = response.json()
 
-                # Check for errors in the API response
                 if 'error' in data:
                     error = data['error'].get('message', 'An unknown error occurred. Please try again.')
                 else:
@@ -35,16 +40,10 @@ def index():
                         'humidity': data['current']['humidity'],
                         'feels_like': data['current']['feelslike_c']
                     }
-            except requests.exceptions.HTTPError as http_err:
-                error = f"HTTP error occurred: {http_err}"
             except requests.exceptions.RequestException as req_err:
                 error = f"Request error occurred: {req_err}"
-            except ValueError:
-                error = "Received an invalid response from the weather service."
 
     return render_template('index.html', weather=weather, error=error)
-  
-
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
